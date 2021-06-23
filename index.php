a3c72cd21c47d97efeb889d057fae476e75e9863
Rob Nugen <rob@robnugen.com>
2021-06-23 16:10:33 -0700

    Sort files so most recent are on top of list

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   </head>
   <body>
     <h1>Babychan's Index of /pdf</h1>
-    <h3>Look for the newest files at the bottom</h3>
+    <h3>Look for the newest files at the top</h3>
     <h3>I love you!  BooooooOOOOoooooooffff!!!</h3>
     <pre>      <a href="?C=N;O=D">Name</a>                           <a href="?C=M;O=A">Last modified</a>
 <?php
@@ -29,7 +29,8 @@
     }
   }
 
-  asort($found_files_array);   // automagically sorts them by key so they end up with newest files at the bottom
+  // NOT Used since 2021 June 24: asort($found_files_array);   // automagically sorts them by key so they end up with newest files at the bottom
+  rsort($found_files_array);   // automagically sorts them by key so they end up with newest files at the top
   foreach($found_files_array as $found_file)
   {
       echo '      <a href="' . $found_file['name'] . '">' . $found_file['name'] . '</a> ' . $found_file['date'] . "\n";
